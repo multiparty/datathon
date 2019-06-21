56495c5ded9a37e0ea524bc136c17ae68b39710b
lucyq <lucyzqin@gmail.com>
2019-06-21 12:46:39 -0400

    progress

--- parsing_scripts/trial_1.py
+++ parsing_scripts/trial_1.py
@@ -1,12 +1,10 @@
 import glob
 import csv
+import json
 
 filenames = glob.glob("../synthetic_data/trial_1/*.csv");
 
 MASK_ID = 0;
-
-print(glob)
-print(filenames);
 
 parsed_file = {};
 
@@ -28,12 +26,26 @@ for f in filenames:
         field = hdr[i];
 
         if field not in parsed_file[patient_id]:
-          parsed_file[patient_id][field] = [];
+          # print(field);
+          patient_row = parsed_file[patient_id];
+          patient_row[field] = {};
 
-        parsed_file[patient_id][field].append(value);
 
-for patient in parsed_file:
-  print(field)
+        #   parsed_file[patient_id][field] = [];
+          # parsed_file[patient_id][field] = [];
+          # print(field)
+
+        # parsed_file[patient_id][field].append(value);
+
+with open('../parsed_data/trial1_parsed.json', 'wb') as json_file:
+  json.dump(parsed_file, json_file);
+
+
+# print(parsed_file)
+# for patient in parsed_file:
+#   for field in parsed_file[patient]:
+#   # print(field) .
+#     print(field, parsed_file[patient][field])
   # for field in patient:
     # # if len(parsed_file[patient][field]) === 1:
     #   print
